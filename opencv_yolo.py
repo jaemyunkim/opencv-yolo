534f1c9bfac4dc58cdd6bdd84f7032fd59d92506
Jaemyun Kim <jaemyunkim@outlook.com>
2023-11-08 18:06:25 +0900

    add yolo model in read_image.py

--- opencv_yolo.py
+++ opencv_yolo.py
@@ -11,20 +11,36 @@ def main():
     YOLO_MODEL = "yolov3"
     YOLO_DATA = "coco"
 
-    # get yolo model files
-    get_yolo_weights(YOLO_MODEL)
-
     # initialize YOLO model
     net, classes, output_layers = initialize(YOLO_MODEL, YOLO_DATA)
-    colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
     # read image
     image = cv2.imread("samples/image1.jpg")
-    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     height, width, channels = image.shape
 
+    # predict objects
+    indexes, class_ids, confidences, boxes = predict(image, net, output_layers, (width, height))
+
+    # draw result
+    resImg = image.copy()
+    drawPred(resImg, indexes, class_ids, confidences, boxes, classes)
+
+    # write and display the result to a file
+    cv2.imwrite("result.png", resImg)
+    cv2.imshow("image", image)
+    cv2.imshow("result", resImg)
+    cv2.waitKey(0)
+
+
+def predict(image, net, output_layers, size):
+    width = size[0]
+    height = size[1]
+
+    # revert channels
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
     # prepare run the detection
-    blob = cv2.dnn.blobFromImage(img, 1/256, (416, 416), (0, 0, 0), swapRB=True, crop=False)
+    blob = cv2.dnn.blobFromImage(image, 1/256, (416, 416), (0, 0, 0), swapRB=True, crop=False)
     net.setInput(blob)
 
     # run the forward pass to get a predict result
@@ -33,32 +49,20 @@ def main():
     # post-processing
     indexes, class_ids, confidences, boxes = postprocessing(outs, (width, height))
 
-    # draw result
-    resImg = image.copy()
-    font = cv2.FONT_HERSHEY_PLAIN
-    colors = np.random.uniform(0, 255, size=(len(boxes), 3))
-    if len(indexes) > 0:
-        for i in indexes.flatten():
-            x, y, w, h = boxes[i]
-            print(x, y, w, h)
-            label = str(classes[class_ids[i]])
-            confidence = str(round(confidences[i], 2))
-            color = colors[i]
-            cv2.rectangle(resImg, (x, y), ((x + w), (y + h)), color, 2)
-            cv2.putText(resImg, label + " " + confidence, (x, y + 20), font, 2, (0, 255, 0), 2)
-
-    # write the result to a file
-    cv2.imwrite("result.png", resImg)
-    cv2.waitKey(1)
+    return indexes, class_ids, confidences, boxes
 
 
-def initialize(yolo_model, yolo_data):
-    yolo_model = yolo_model.lower()
-    yolo_data = yolo_data.lower()
+def initialize(model_name, data_name):
+    model_name = model_name.lower()
+    data_name = data_name.lower()
 
-    net = cv2.dnn.readNet(f"models/{yolo_model}.weights", f"models/{yolo_model}.cfg")
+    # get yolo model files
+    get_yolo_weights(model_name)
+
+    # load yolo model
+    net = cv2.dnn.readNet(f"models/{model_name}.weights", f"models/{model_name}.cfg")
     classes = []
-    with open(f"models/{yolo_data}.names", "r") as f:
+    with open(f"models/{data_name}.names", "r") as f:
         classes = [line.strip() for line in f.readlines()]
     layer_names = net.getLayerNames()
     output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
@@ -96,6 +100,22 @@ def postprocessing(outs, size):
     indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
 
     return indexes, class_ids, confidences, boxes
+
+
+def drawPred(image, indexes, class_ids, confidences, boxes, classes):
+    font = cv2.FONT_HERSHEY_PLAIN
+    colors = np.random.uniform(0, 255, size=(len(classes), 3))
+    # colors = np.random.uniform(0, 255, size=(len(boxes), 3))
+    if len(indexes) > 0:
+        for i in indexes.flatten():
+            x, y, w, h = boxes[i]
+            print(x, y, w, h)
+            label = str(classes[class_ids[i]])
+            confidence = str(round(confidences[i], 2))
+            color = colors[class_ids[i]]
+            # color = colors[i]
+            cv2.rectangle(image, (x, y), ((x + w), (y + h)), color, 2)
+            cv2.putText(image, label + " " + confidence, (x, y + 20), font, 2, (0, 255, 0), 2)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 import requests
+from tqdm import tqdm
 
 
 def get_yolo_weights(model_name):
@@ -24,14 +25,25 @@ def get_yolo_weights(model_name):
         target_file = Path(f"models/{Path(url).name}")
         if not target_file.exists():
             target_file.parent.mkdir(parents=True, exist_ok=True)   # create directory
-            r = requests.get(url) # create HTTP response object
-            with open(str(target_file), "wb") as f: 
+            
+            chunk_size = 1024
+            r = requests.get(url, stream=True) # create HTTP response object
+            total = int(r.headers.get('content-length', 0))
+            with open(str(target_file), "wb") as f, tqdm(
+                desc=str(target_file),
+                total=total,
+                unit='iB',
+                unit_scale=True,
+                unit_divisor=chunk_size,
+            ) as bar: 
                 # Saving received content as a png file in 
                 # binary format 
             
                 # write the contents of the response (r.content) 
                 # to a new file in binary mode. 
-                f.write(r.content)
+                for data in r.iter_content(chunk_size=chunk_size):
+                    size = f.write(data)
+                    bar.update(size)
             
             print(f"download file: {str(target_file)}")
         else:
